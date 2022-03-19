48b3eb334f1ac4812ea76aef52f94e5fd885ada3
Shalah <zoungranaala@gmail.com>
2022-03-18 20:01:54 -0400

    created a dist file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,56 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor',
+
+      }),
+
+      // new InjectManifest({
+      //   // These are some common options, and not all are required.
+      //   // Consult the docs for more info.
+      //   // exclude: [/.../, '...'],
+      //   // maximumFileSizeToCacheInBytes: ...,
+      //   swSrc: '../',
+      // }),
+
+      new InjectManifest(),
+      new WebpackPwaManifest({
+        name: 'Text Editor',
+        short_name: 'Text Editor',
+        description: 'Remember important tasks!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: '/',
+        publicPath: '/',
+        // icons: [
+        //   {
+        //     src: path.resolve('assets/images/logo.png'),
+        //     sizes: [96, 128, 192, 256, 384, 512],
+        //     destination: path.join('assets', 'icons'),
+        //   },
+        // ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
