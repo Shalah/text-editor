d65539be2f10b7080475cde77c21f78a3d5d1f81
Shalah <zoungranaala@gmail.com>
2022-03-19 15:05:02 -0400

    Fixing bugs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,18 +16,17 @@ const initdb = async () =>
 export const putDb = async (content) => {
   console.error('putDb not implemented');
 
-
   // Create a connection to the database database and version we want to use.
-  const contenttDb = await openDB('contact', 1);
+  const jatetDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readwrite');
+  const tx = jatetDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
   // Use the .delete() method to get all data in the database.
-  const request = store.delete(id);
+  const request = store.put({id: id, jate:content});
 
   // Get confirmation of the request.
   const result = await request;
@@ -41,21 +40,21 @@ export const getDb = async () => {
   console.error('getDb not implemented');
 
   // Create a connection to the database database and version we want to use.
-  const contentDb = await openDB('content', 1);
+  const jatetDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contentDb.transaction('content', 'readwrite');
+  const tx = jatetDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('content');
+  const store = tx.objectStore('jate');
 
   // Use the .delete() method to get all data in the database.
-  const request = store.delete(id);
+  const request = store.getAll();
 
   // Get confirmation of the request.
   const result = await request; 
   console.log('result.value', result);
-  return result?.value;
+  return result; 
 }
 
 initdb();
